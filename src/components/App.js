9294e930e473745d8f9f6c6f147c776fbbbe4402
Satyam Raj <sstymrj@gmail.com>
2025-09-26 16:01:18 +0530

    Implement sum calculator with state management
    Added state management for count and sum using useState and useEffect.

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,28 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [count, setCount] = useState(0)
+  const [sum, setSum] = useState(0)
+
+  useEffect(()=>{
+    setSum(sum=>sum+count)
+  },[count])
+  
   return (
     <div>
         {/* Do not remove the main div */}
+
+          <p>Sum Calculator</p>
+      <input
+      onChange={(e)=>{
+        const val = parseInt(e.target.value)
+        setCount(val)
+      }}
+      type="number"/>
+
+
+      <h1>{sum}</h1>
     </div>
   )
 }
